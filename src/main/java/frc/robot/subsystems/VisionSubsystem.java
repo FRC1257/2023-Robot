124f6bdbccb0907781c7bf545415db7e4a0abeaf
derp000 <84947701+derp000@users.noreply.github.com>
2023-01-11 15:52:57 -0500

    Add VisionSubsystem.java

---
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -0,0 +1,73 @@
+// Author: UMN Robotics Ri3d
+// Last Updated : January 2023
+
+package frc.robot.subsystems;
+
+import frc.robot.Constants;
+
+import java.util.List;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class VisionSubsystem extends SnailSubsystem {
+    PhotonCamera camera = new PhotonCamera(Constants.USB_CAMERA_NAME); // Declare the name of the camera used in the pipeline
+    boolean hasTarget; // Stores whether or not a target is detected
+    PhotonPipelineResult result; // Stores all the data that Photonvision returns
+
+    // @Override
+    // public void periodic() {
+    //     PhotonPipelineResult result = camera.getLatestResult(); // Query the latest result from PhotonVision
+    //     hasTarget = result.hasTargets(); // If the camera has detected an apriltag target, the hasTarget boolean will be true
+    //     if (hasTarget) {
+    //         this.result = result;
+    //     }
+    // }
+    public PhotonTrackedTarget getTargetWithID(int id) { // Returns the apriltag target with the specified ID (if it exists)
+        List<PhotonTrackedTarget> targets = result.getTargets(); // Create a list of all currently tracked targets
+        for (PhotonTrackedTarget i : targets) {
+            if (i.getFiducialId() == id) { // Check the ID of each target in the list
+                return i; // Found the target with the specified ID!
+            }
+        }
+        return null; // Failed to find the target with the specified ID
+    }
+    
+    public PhotonTrackedTarget getBestTarget() {
+        if (hasTarget) {
+        return result.getBestTarget(); // Returns the best (closest) target
+        }
+        else {
+            return null; // Otherwise, returns null if no targets are currently found
+        }
+    }
+    public boolean getHasTarget() {
+        return hasTarget; // Returns whether or not a target was found
+    }
+    @Override
+    public void update() {
+        PhotonPipelineResult result = camera.getLatestResult(); // Query the latest result from PhotonVision
+        hasTarget = result.hasTargets(); // If the camera has detected an apriltag target, the hasTarget boolean will be true
+        if (hasTarget) {
+            this.result = result;
+        }
+    }
+    @Override
+    public void displayShuffleboard() {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void tuningInit() {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void tuningPeriodic() {
+        // TODO Auto-generated method stub
+        
+    }
+}
