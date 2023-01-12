d73f12a5eff4e5fe6234bdca8a09cad1c0bbebf7
FlamingMan45 <95654442+FlamingMan45@users.noreply.github.com>
2023-01-12 16:18:39 -0500

    Update generateTrajedies.java

--- src/main/java/frc/robot/commands/generateTrajedies.java
+++ src/main/java/frc/robot/commands/generateTrajedies.java
@@ -1,8 +1,11 @@
 package frc.robot.commands;
 import toCharge;
 
-public class generateTrajedies{
-
- 
+public class generateTrajedies {
+ private boolean charge; 
+ private boolean score
+ private boolean cargo; 
+ private final Pose2d StartPos;
+ private final Pose2d NewPos;
 
 }
