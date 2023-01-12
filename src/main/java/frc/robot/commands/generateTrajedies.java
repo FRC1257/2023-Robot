aa23651b2773e25d2ee5af155e8b8adbf744ebf9
FlamingMan45 <95654442+FlamingMan45@users.noreply.github.com>
2023-01-12 17:45:08 -0500

    Update generateTrajedies.java

--- src/main/java/frc/robot/commands/generateTrajedies.java
+++ src/main/java/frc/robot/commands/generateTrajedies.java
@@ -7,15 +7,17 @@ public class generateTrajedies extends SequentialCommandGroup {
  private boolean charge; 
  private boolean score;
  private boolean cargo; 
+ private Drivetrain driveTrain; 
  private double maxVelocity;
  private double minVelocity;
  private final Pose2d StartPos;
  private final Pose2d NewPos;
  
-  public generateTrajedies (boolean charge, boolean score, boolean cargo, double maxVelocity, double minVelocity, Pose2d StartPos, Pose2d NewPos) {
+  public generateTrajedies (boolean charge, boolean score, boolean cargo, Drivetrain driveTrain, double maxVelocity, double minVelocity, Pose2d StartPos, Pose2d NewPos) {
    this.charge = charge;
    this.score = score;
    this.cargo = cargo;
+   this.driveTrain = driveTrain
    this.maxVelocity = maxVelocity;
    this.minVelocity = minVelocity;
    this.StartPos = StartPos;
@@ -24,15 +26,15 @@ public class generateTrajedies extends SequentialCommandGroup {
   }
  public void TrajediesDecider() {
    if (this.score){
-    ToScoreCommands obj1 = new ToScoreCommands(this.StartPos);  
+    ToScoreCommands obj1 = new ToScoreCommands(this.driveTrain, this.StartPos, this.NewPos);  
     
      } 
    if (this.cargo){
-    ToCargoCommands obj1 = new ToCargoCommands(this.StartPos, this.NewPos);
+    ToCargoCommands obj1 = new ToCargoCommands(this.driveTrain, this.StartPos, this.NewPos);
     
       }
    if (this.charge){
-    ToChargeCommands obj1 = new ToChargeCommands(this.StartPos);
+    ToChargeCommands obj1 = new ToChargeCommands(this.driveTrain, this.StartPos);
     
       }
    if (this.StartPos != this.NewPos){
