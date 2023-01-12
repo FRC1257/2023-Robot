e6826a1fe54bfed2d5295199940883aacfcf1afe
ipad6202 <78675126+ipad6202@users.noreply.github.com>
2023-01-12 17:21:17 -0500

    Update ToChargeCommand.java

--- src/main/java/frc/robot/commands/ToChargeCommand.java
+++ src/main/java/frc/robot/commands/ToChargeCommand.java
@@ -1,5 +1,16 @@
 package frc.robot.commands;
-public class ToChargeCommand {
-  
+import java.util.List;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.Constants.Drivetrain.*;
+
+public class ToChargeCommand extends CommandBase {
+  private final Pos2d CurrentPosition;
+  private final Drivetrain
   
 } 
