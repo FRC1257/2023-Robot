b419bb9513fe3102b9779162b2dc361365d28f54
FlamingMan45 <95654442+FlamingMan45@users.noreply.github.com>
2023-01-12 17:52:51 -0500

    Update ToChargeCommand.java

--- src/main/java/frc/robot/commands/ToChargeCommand.java
+++ src/main/java/frc/robot/commands/ToChargeCommand.java
@@ -10,7 +10,50 @@ import frc.robot.subsystems.Drivetrain;
 import frc.robot.Constants.Drivetrain.*;
 
 public class ToChargeCommand extends CommandBase {
-  private final Pos2d CurrentPosition;
-  private final Drivetrain
+    private final Drivetrain drivetrain;
+    private final Pose2d startPose;
+
+    public ToChargeCommand(Drivetrain drivetrain, Pose2d startPose) {
+        this.drivetrain = drivetrain;
+        this.startPose = startPose;
+
+        addRequirements(drivetrain);
+    }
+
+    @Override
+    public void initialize() {
+        //generate trajectory and follow it
+        //must choose at least one waypoint on one side of the charging station, depending on where the end location is
+        // might get stuck on charging station or waste time
+        List<Pose2d> trajPoints; 
+        //waypoint locations
+        //x= blue 96.75 in or red 484.81 in = 2.45745 m or 12.314174
+        //y= outer 29.695 in or inner 125.695 in  = 0.754253 m or 3.192653 m
+        //double x, y;
+        //center line 107.39 in = 2.727706 m
+        trajPoints.add(startPose);
+        //Pose2d waypoint = new Pose2d(x, y, someRotation);
+        //trajPoints.add(waypoint);
+        trajPoints.add(endPose);
+        TrajectoryConfig config = new TrajectoryConfig(DRIVE_TRAJ_MAX_VEL, DRIVE_TRAJ_MAX_ACC);
+        Trajectory trajectory = TrajectoryGenerator.generateTrajectory(trajPoints, config);
+        drivetrain.driveTrajectory(trajectory);
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        drivetrain.endPID();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return drivetrain.getState() != Drivetrain.State.TRAJECTORY;
+    }
+}
   
-} 
+}
