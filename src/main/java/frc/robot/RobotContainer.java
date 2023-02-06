25af7a4876970dc53de14fc89306d5a5a2d2a4f7
Swordman51 <51swordman@gmail.com>
2023-02-06 15:50:28 -0500

    Revert "removed imports"
    This reverts commit 35b11a40b98fdef798f3fc94bf3d6801952b942d.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,10 +1,19 @@
 package frc.robot;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.Notifier;
+import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.GenerateTrajedies;
+import frc.robot.commands.drivetrain.*;
+import frc.robot.commands.vision.TurnToAprilTagCommand;
+import frc.robot.subsystems.*;
 import frc.robot.subsystems.SnailSubsystem;
 import frc.robot.util.SnailController;
 
@@ -12,7 +21,8 @@ import java.util.ArrayList;
 
 import static frc.robot.Constants.ElectricalLayout.CONTROLLER_DRIVER_ID;
 import static frc.robot.Constants.ElectricalLayout.CONTROLLER_OPERATOR_ID;
-import static frc.robot.Constants.UPDATE_PERIOD;;
+import static frc.robot.Constants.UPDATE_PERIOD;
+import static frc.robot.Constants.Autonomous;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -23,24 +33,38 @@ import static frc.robot.Constants.UPDATE_PERIOD;;
 public class RobotContainer {
 
     private static final String kDefaultAuto = "Default";
-private static final String kCustomAuto = "My Auto";
-private String m_autoSelected;
-private final SendableChooser<String> side_chooser = new SendableChooser<>();
+    private static final String kCustomAuto = "My Auto";
+    private String m_autoSelected;
 
     private SnailController driveController;
     private SnailController operatorController;
     
     private ArrayList<SnailSubsystem> subsystems;
 
+
+    private Drivetrain drivetrain;
+    private Vision vision;
+
     private Notifier updateNotifier;
     private int outputCounter;
+    private int displayTrajCounter;
 
-    SendableChooser<Pose2d> scorePositionChooser = new SendableChooser<>();
-    SendableChooser<Pose2d> gamePieceChooser = new SendableChooser<>(); 
+    private boolean updateTraj = true;
 
-    boolean score;
-    boolean cargo;
-    boolean charge;
+    // choosers
+    public static SendableChooser<Integer> scorePositionChooser = new SendableChooser<>();
+    public static SendableChooser<Integer> gamePieceChooser = new SendableChooser<>(); 
+    public static SendableChooser<Integer> startPositionChooser = new SendableChooser<>(); 
+
+    //booleans regarding the score, cargo, and charge
+    private boolean score;
+    private boolean cargo;
+    private boolean charge;
+
+    private boolean updateVisual = false;
+
+    private GenerateTrajedies generateTrajedies;
+
 
     /**
      * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -49,50 +73,127 @@ private final SendableChooser<String> side_chooser = new SendableChooser<>();
         driveController = new SnailController(CONTROLLER_DRIVER_ID);
         operatorController = new SnailController(CONTROLLER_OPERATOR_ID);
 
-        configureSubsystems();
         configureAutoChoosers();
+        configureSubsystems();
         configureButtonBindings();
         
         outputCounter = 0;
+        displayTrajCounter = 0;
 
-        SmartDashboard.putBoolean("Testing", false);
+        // Field Side
+        SmartDashboard.putBoolean("isAllianceBlue", getAllianceColor());
+        SmartDashboard.putBoolean("Testing", true);
+        //getting the auto values for score, cargo, and charge
         SmartDashboard.putBoolean("Auto Score", score);
         SmartDashboard.putBoolean("Auto Get Cargo", cargo);
         SmartDashboard.putBoolean("Auto Goto Charge", charge);
+        SmartDashboard.putNumber("View Trajectory Pos", 0);
+        SmartDashboard.putBoolean("Update Visual", false);
 
         updateNotifier = new Notifier(this::update);
         updateNotifier.startPeriodic(UPDATE_PERIOD);
     }
 
+    public void stopDisplayingTraj() {
+        updateTraj = false;
+    }
+
+    private boolean getAllianceColor() {
+        return DriverStation.getAlliance() == DriverStation.Alliance.Blue;
+    }
+
+    private Pose2d getStartingPos() {
+        return new Pose2d(0, 0, new Rotation2d(0.0));
+        /* Pose2d[] ALLIANCE_START_POSE;
+        if (SmartDashboard.getBoolean("isAllianceBlue", false)) {
+            ALLIANCE_START_POSE = Autonomous.BLUE_START_POSE;
+        } else {
+            ALLIANCE_START_POSE = Autonomous.RED_START_POSE;
+        }
+        return ALLIANCE_START_POSE[startPositionChooser.getSelected()]; 
+         */
+    }
+
+    /**
+     * Set up the choosers on shuffleboard for getting score positions
+     */
+
+   
     /**
      * Declare all of our subsystems and their default bindings
      */
     private void configureSubsystems() {
         // declare each of the subsystems here
+        drivetrain = new Drivetrain(getStartingPos());
+        // drivetrain.setDefaultCommand(new ManualDriveCommand(drivetrain, driveController::getDriveForward, driveController::getDriveTurn));
+        drivetrain.setDefaultCommand(new VelocityDriveCommand(drivetrain, driveController::getDriveForward, driveController::getDriveTurn,
+             driveController.getButton(Button.kLeftBumper.value)::getAsBoolean, false));
+
+        // Vision
+        vision = new Vision();
 
         subsystems = new ArrayList<>();
         // add each of the subsystems to the arraylist here
+        subsystems.add(drivetrain);
+        subsystems.add(vision);
+
+        // generate auto
+        generateTrajedies = new GenerateTrajedies(
+            drivetrain,
+            charge,
+            score,
+            cargo,
+            drivetrain,
+            0
+        );
     }
 
     /**
-     * Define button -> command mappings.
+     * Define {@link Button} -> command mappings.
      */
     private void configureButtonBindings() {
-        
+        // Drivetrain bindings
+        driveController.getButton(Button.kY.value).onTrue(new ToggleReverseCommand(drivetrain));
+        driveController.getButton(Button.kStart.value).onTrue(new ToggleSlowModeCommand(drivetrain));
+        //driveController.getButton(Button.kA.value).onTrue(new TurnAngleCommand(drivetrain, -90));
+        //driveController.getButton(Button.kB.value).onTrue(new TurnAngleCommand(drivetrain, 90));
+        driveController.getButton(Button.kX.value).onTrue(new ResetDriveCommand(drivetrain));
+        driveController.getButton(Button.kLeftBumper.value).onTrue(new TurnToAprilTagCommand(drivetrain, vision));
     }
 
     /**
      * Set up the choosers on shuffleboard for autonomous
      */
     public void configureAutoChoosers() {
-        
+        configureGamePieceChooser();
+        configureScorePositionChooser();
+        configureStartPositionChooser();
+    }
+
+    private int estimatedCurrentPose2d() {
+        // TODO: Implement this with PhotonVision
+        // return new Pose2d(0, 0, new Rotation2d(0.0));
+        return startPositionChooser.getSelected();
     }
 
     /**
      * Do the logic to return the auto command to run
      */
     public Command getAutoCommand() {
-        return null;
+        updateAutoChoosers();
+
+        generateTrajedies = new GenerateTrajedies(
+            drivetrain,
+            charge,
+            score,
+            cargo,
+            drivetrain,
+            estimatedCurrentPose2d()
+        );
+        
+        // drivetrain.drawTrajectory(generateTrajedies.getTrajectory());
+        // DriverStation.reportWarning("Auto Command: " + generateTrajedies.getTrajectory().toString(), false);
+        return generateTrajedies.getCommand();
     }
 
     /**
@@ -129,9 +230,87 @@ private final SendableChooser<String> side_chooser = new SendableChooser<>();
         if(outputCounter % 3 == 0) {
             subsystems.get(outputCounter / 3).tuningPeriodic();
         }
+
+
+        if (updateTraj) { // change the trajectory drawn
+            // generateTrajedies.incrementOutputCounter();
+            Trajectory traj = generateTrajedies.getTrajectory((int)SmartDashboard.getNumber("View Trajectory Pos", 0));
+            if (traj != null)
+                drivetrain.drawTrajectory(traj);
+        }
+
+        if (updateTraj && checkIfUpdate()) {
+            DriverStation.reportWarning("Updating Auto", cargo);
+            updateAutoChoosers();
+
+            generateTrajedies = new GenerateTrajedies(
+                drivetrain,
+                charge,
+                score,
+                cargo,
+                drivetrain,
+                estimatedCurrentPose2d()
+            );
+            resetDashboard();
+        }
+    
+    }
+
+    //sendable chooser methods
+
+    public void configureGamePieceChooser() {
+        gamePieceChooser.setDefaultOption("-1", 0);
+        gamePieceChooser.addOption("1st Position", 0);
+        gamePieceChooser.addOption("2nd Position", 1);
+        gamePieceChooser.addOption("3rd Position", 2);
+        gamePieceChooser.addOption("4th Position", 3);
+        SmartDashboard.putData(gamePieceChooser);
+    }
+    
+  
+    public void configureScorePositionChooser() {
+        scorePositionChooser.setDefaultOption("-1", 0);
+        scorePositionChooser.addOption("1st Position", 0);
+        scorePositionChooser.addOption("2nd Position", 1);
+        scorePositionChooser.addOption("3rd Position", 2);
+        scorePositionChooser.addOption("4th Position", 3);
+        scorePositionChooser.addOption("5th Position", 4);
+        scorePositionChooser.addOption("6th Position", 5);
+        scorePositionChooser.addOption("7th Position", 6);
+        scorePositionChooser.addOption("8th Position", 7);
+        scorePositionChooser.addOption("9th Position", 8);
+        SmartDashboard.putData(scorePositionChooser);
+    }
+
+    public void configureStartPositionChooser() {
+        startPositionChooser.setDefaultOption("Set me", 0);
+        startPositionChooser.addOption("1st Position", 0);
+        startPositionChooser.addOption("2nd Position", 1);
+        startPositionChooser.addOption("3rd Position", 2);
+        SmartDashboard.putData(startPositionChooser);
     }
 
 
-  
+    public boolean checkIfUpdate() {
+        return score != SmartDashboard.getBoolean("Auto Score", false) || cargo != SmartDashboard.getBoolean("Auto Get Cargo", false) || charge != SmartDashboard.getBoolean("Auto Goto Charge", false) || SmartDashboard.getBoolean("Update Visual", false);
+    }
+
+    public void updateAutoChoosers() {
+        score = SmartDashboard.getBoolean("Auto Score", false);
+        cargo = SmartDashboard.getBoolean("Auto Get Cargo", false);
+        charge = SmartDashboard.getBoolean("Auto Goto Charge", false);
+    }
+
+    public void resetDashboard() {
+        // Field Side
+        SmartDashboard.putBoolean("isAllianceBlue", getAllianceColor());
+        
+        //getting the auto values for score, cargo, and charge
+        SmartDashboard.putBoolean("Auto Score", score);
+        SmartDashboard.putBoolean("Auto Get Cargo", cargo);
+        SmartDashboard.putBoolean("Auto Goto Charge", charge);
+        SmartDashboard.putNumber("View Trajectory Pos", (int)SmartDashboard.getNumber("View Trajectory Pos", 0));
+        SmartDashboard.putBoolean("Update Visual", false);
+    }
 
 }
