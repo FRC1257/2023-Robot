0934a487dce2543fa67faa2be83c4bd915de892d
coldcookies <73250770+coldcookies@users.noreply.github.com>
2023-02-09 17:50:51 -0500

    Create Roller.java

---
+++ src/main/java/frc/robot/subsystems/Roller.java
@@ -0,0 +1,42 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+public class Roller extends SnailSubsystem {
+
+    public enum State {
+      INTAKING,
+      EJECTING,
+      NEUTRAL
+    }
+
+    public Roller() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void displayShuffleboard() {
+
+    }
+
+    @Override
+    public void tuningInit() {
+
+    }
+
+    @Override
+    public void tuningPeriodic() {
+
+    }
+
+    public State getState() {
+        return state;
+    }
+}
