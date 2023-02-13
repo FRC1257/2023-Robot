76fa0fa92da327b1f8b7219c948cb70980af3f9e
coldcookies <73250770+coldcookies@users.noreply.github.com>
2023-02-13 15:16:46 -0500

    Update ClawAlt.java

--- src/main/java/frc/robot/subsystems/ClawAlt.java
+++ src/main/java/frc/robot/subsystems/ClawAlt.java
@@ -3,27 +3,49 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import java.frc.robot.Constants.*;
 
 public class ClawAlt extends SnailSubsystem {
-
+    private CANSparkMax motorLeft;
+    private CANSparkMax motorRight;
     public enum State {
       INTAKING,
       EJECTING,
       NEUTRAL
     }
-
+    
+    private State state = State.NEUTRAL;
+    
     public ClawAlt() {
-
+        motorLeft = new CANSparkMax(CLAW_MOTOR_LEFT, MotorType.kBrushless);
+        motorRight = new CANSparkMax(CLAW_MOTOR_RIGHT, MotorType.kBrushless);
+        motorInit(motorLeft);
+        motorInit(motorRight);
+        motorRight.follow(motorLeft, true);
     }
-
+    private void motorInit(CANSparkMax motor) {
+        motor.restoreFactoryDefaults();
+        motor.setIdleMode(IdleMode.kBrake);
+        motor.setSmartCurrentLimit(NEO_550_CURRENT_LIMIT);
+    }
     @Override
     public void update() {
-
+        switch(state) {
+            case NEUTRAL:
+                motorLeft.set(0.0);
+                break;
+            case INTAKING:
+                motorLeft.set(0.85);
+                break;
+            case EJECTING:
+                motorLeft.set(-0.85);
+                break;
+        }
     }
 
     @Override
     public void displayShuffleboard() {
-
+        
     }
 
     @Override
@@ -36,6 +58,18 @@ public class ClawAlt extends SnailSubsystem {
 
     }
 
+    public void neutral() {
+        state = State.NEUTRAL;
+    }
+
+    public void eject() {
+        state = State.EJECTING;
+    }
+
+    public void intake() {
+        state = State.INTAKING;
+    }
+    
     public State getState() {
         return state;
     }
