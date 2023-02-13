1563054a77435589e8e33acc8641325b9918bc85
tehepix2 <101379979+tehepix2@users.noreply.github.com>
2023-02-13 17:33:43 -0500

    Started PivotArm commands - added PIDCommand file - started regular command

---
+++ src/main/java/frc/robot/commands/pivotArmCommands/PivotArmManualCommand.java
@@ -0,0 +1,37 @@
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.PivotArm;
+
+public class PivotArmManualCommand extends CommandBase {
+    
+    private PivotArm pivotArm;
+    
+
+    public PivotArmManualCommand(PivotArm pivotArm) {
+        this.pivotArm = pivotArm;
+        
+        addRequirements(pivotArm);
+    }
+    
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+
+    }
+}
