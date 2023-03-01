474364565729e5e7aaff9e90dea653a962c7ac15
ipad6202 <78675126+ipad6202@users.noreply.github.com>
2023-03-01 16:18:28 -0500

    HighScoreCompound Command Finished
    Very cool high thingy

--- src/main/java/frc/robot/commands/compound_commands/HighScoreCommand.java
+++ src/main/java/frc/robot/commands/compound_commands/HighScoreCommand.java
@@ -1,5 +1,28 @@
 package frc.robot.commands.compound_commands;
 
-public class HighScoreCommand {
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import static frc.robot.Constants.Pivotwrist.WRIST_SETPOINT_TOP;
+import static frc.robot.Constants.PivotArm.PIVOT_ARM_SETPOINT_UP;
+
+import frc.robot.commands.elevator.ElevatorExtendCommand;
+import frc.robot.commands.pivotArm.PivotArmPIDCommand;
+import frc.robot.commands.pivotWrist.PivotWristPIDCommand;
+import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.PivotArm;
+import frc.robot.subsystems.PivotWrist;
+
+public class HighScoreCommand extends SequentialCommandGroup{
+    
+    public HighScoreCommand(Elevator elevator, PivotArm pivotarm, PivotWrist pivotwrist) {
+        
+        addCommands(
+            new ElevatorExtendCommand(elevator),
+            new ParallelCommandGroup(
+                new PivotWristPIDCommand(pivotwrist, WRIST_SETPOINT_TOP),
+                new PivotArmPIDCommand(pivotarm, PIVOT_ARM_SETPOINT_UP)
+            )
+        );
+    }
     
 }
