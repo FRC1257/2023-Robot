d8a4e320e47fba525694d9fdee9f621111d4a27d
ipad6202 <78675126+ipad6202@users.noreply.github.com>
2023-03-01 16:21:25 -0500

    Fixed Constant Name

--- src/main/java/frc/robot/commands/compound_commands/HighScoreCommand.java
+++ src/main/java/frc/robot/commands/compound_commands/HighScoreCommand.java
@@ -2,7 +2,7 @@ package frc.robot.commands.compound_commands;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import static frc.robot.Constants.Pivotwrist.WRIST_SETPOINT_TOP;
+import static frc.robot.Constants.PivotWrist.WRIST_SETPOINT_TOP;
 import static frc.robot.Constants.PivotArm.PIVOT_ARM_SETPOINT_UP;
 
 import frc.robot.commands.elevator.ElevatorExtendCommand;
