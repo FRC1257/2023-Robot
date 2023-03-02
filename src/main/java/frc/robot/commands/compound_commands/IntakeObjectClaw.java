c1bd93f57d028f838b1271995555b50a28b5703c
Akash Dubey <69588353+AkeBoss-tech@users.noreply.github.com>
2023-03-02 15:55:07 -0500

    Updated intake commands

--- src/main/java/frc/robot/commands/compound_commands/IntakeObjectClaw.java
+++ src/main/java/frc/robot/commands/compound_commands/IntakeObjectClaw.java
@@ -8,14 +8,15 @@ import frc.robot.Constants;
 import frc.robot.subsystems.*;
 
 public class IntakeObjectClaw extends SequentialCommandGroup{
-    public IntakeObjectClaw(PivotArm pivotarm, PivotWrist pivtowrist, Intake intake, IntakeArm intakeArm, Elevator elevator) {
+    public IntakeObjectClaw(PivotArm pivotarm, PivotWrist pivotwrist, Intake intake, IntakeArm intakeArm, Elevator elevator) {
         addCommands( //WITH INTAKE
             //new ElevatorExtendCommand(elevator),
             new ParallelCommandGroup(
                 new IntakeIntakingCommand(intake), // run intake entire time
                 new ElevatorExtendCommand(elevator),
                 new SequentialCommandGroup(
-                    new IntakeArmPIDCommand(intakeArm, Constants.IntakeArm.INTAKE_SETPOINT_BOT)
+                    new IntakeArmPIDCommand(intakeArm, Constants.IntakeArm.INTAKE_SETPOINT_BOT),
+                    new IntakeWristPIDCommand(pivotwrist, Constants.IntakeWrist.INTAKE_SETPOINT_BOT)
                 )
             )
         );
--- src/main/java/frc/robot/commands/compound_commands/IntakeObjectIntake.java
+++ src/main/java/frc/robot/commands/compound_commands/IntakeObjectIntake.java
@@ -2,20 +2,25 @@ package frc.robot.commands.compound_commands;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.elevator.ElevatorRetractCommand;
 import frc.robot.commands.intake.IntakeIntakingCommand;
 import frc.robot.commands.intakearm.IntakeArmPIDCommand;
+import frc.robot.commands.pivotArm.PivotArmPIDCommand;
+import frc.robot.commands.pivotWrist.PivotWristPIDCommand;
 import frc.robot.Constants;
 import frc.robot.subsystems.*;
 
 public class IntakeObjectIntake extends SequentialCommandGroup{
 
-    public IntakeObjectIntake(PivotArm pivotarm, PivotWrist pivtowrist, Intake intake, IntakeArm intakeArm, Elevator elevator) {
+    public IntakeObjectIntake(PivotArm pivotarm, PivotWrist pivotwrist, Intake intake, IntakeArm intakeArm, Elevator elevator) {
         addCommands( //WITH INTAKE
-            //new ElevatorExtendCommand(elevator),
+            new ElevatorRetractCommand(elevator),
             new ParallelCommandGroup(
                 new IntakeIntakingCommand(intake), // run intake entire time
                 new SequentialCommandGroup(
-                    new IntakeArmPIDCommand(intakeArm, Constants.IntakeArm.INTAKE_SETPOINT_BOT)
+                    new IntakeArmPIDCommand(intakeArm, Constants.IntakeArm.INTAKE_SETPOINT_BOT),
+                    new PivotArmPIDCommand(pivotarm, Constants.PivotArm.PIVOT_ARM_SETPOINT_INTAKE),
+                    new PivotWristPIDCommand(pivotwrist, Constants.PivotWrist.WRIST_SETPOINT_INTAKE)
                 )
             )
         );
