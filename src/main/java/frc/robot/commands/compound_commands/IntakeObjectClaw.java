8c9816da9e8bfe6a3fd545c4c167cdef54a7d21a
Swordman Zhao <51swordman@gmail.com>
2023-03-02 16:55:29 -0500

    Finished INtake without Intake

--- src/main/java/frc/robot/commands/compound_commands/IntakeObjectClaw.java
+++ src/main/java/frc/robot/commands/compound_commands/IntakeObjectClaw.java
@@ -1,25 +1,30 @@
 package frc.robot.commands.compound_commands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.claw.ClawIntakeCommand;
 import frc.robot.commands.elevator.ElevatorExtendCommand;
 import frc.robot.commands.intake.IntakeIntakingCommand;
 import frc.robot.commands.intakearm.IntakeArmPIDCommand;
+import frc.robot.commands.pivotArm.PivotArmPIDCommand;
+import frc.robot.commands.pivotWrist.PivotWristPIDCommand;
 import frc.robot.Constants;
 import frc.robot.subsystems.*;
 
 public class IntakeObjectClaw extends SequentialCommandGroup{
-    public IntakeObjectClaw(PivotArm pivotarm, PivotWrist pivotwrist, Intake intake, IntakeArm intakeArm, Elevator elevator) {
+    public IntakeObjectClaw(PivotArm pivotarm, PivotWrist pivotwrist, Intake intake, IntakeArm intakeArm, Elevator elevator, Claw claw) {
         addCommands( //WITH INTAKE
             //new ElevatorExtendCommand(elevator),
-            new ParallelCommandGroup(
-                new IntakeIntakingCommand(intake), // run intake entire time
+            new ParallelCommandGroup( // run intake entire time
                 new ElevatorExtendCommand(elevator),
                 new SequentialCommandGroup(
-                    new IntakeArmPIDCommand(intakeArm, Constants.IntakeArm.INTAKE_SETPOINT_BOT),
-                    new IntakeWristPIDCommand(pivotwrist, Constants.IntakeWrist.INTAKE_SETPOINT_BOT)
+                    new PivotArmPIDCommand(pivotarm, Constants.PivotArm.PIVOT_ARM_SETPOINT_INTAKE),
+                    new PivotWristPIDCommand(pivotwrist, Constants.PivotWrist.WRIST_SETPOINT_INTAKE),
+                    new ClawIntakeCommand(claw)
                 )
-            )
-        );
+                
+                )
+               
+            );
     }
 
  
