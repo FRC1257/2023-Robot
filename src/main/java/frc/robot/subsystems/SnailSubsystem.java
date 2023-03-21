03ec9c79dcd8f78845249abdfc044b6e9c7e3c96
derp000 <84947701+derp000@users.noreply.github.com>
2023-03-20 22:07:43 -0400

    Add javadoc to SnailSubsystem.java

--- src/main/java/frc/robot/subsystems/SnailSubsystem.java
+++ src/main/java/frc/robot/subsystems/SnailSubsystem.java
@@ -7,8 +7,23 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  */
 public abstract class SnailSubsystem extends SubsystemBase {
 
+    /**
+     * Runs code every 20 ms
+     */
     public abstract void update();
+
+    /**
+     * Put data onto SmartDashboard
+     */
     public abstract void displayShuffleboard();
+
+    /**
+     * Send intial PID and other tuning variables for each subsystem to SmartDashboard
+     */
     public abstract void tuningInit();
+    
+    /**
+     * Get data from SmartDashboard
+     */
     public abstract void tuningPeriodic();
 }
