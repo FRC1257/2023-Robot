3f25679c5830cf52ca0173a751317f97d566027f
Swordman Zhao <51swordman@gmail.com>
2023-03-23 19:33:29 -0400

    switched clawintake to intakeintake

--- src/main/java/frc/robot/commands/Compound_Commands/MidScoreShootingCommand.java
+++ src/main/java/frc/robot/commands/Compound_Commands/MidScoreShootingCommand.java
@@ -1,9 +1,10 @@
 package frc.robot.commands.Compound_Commands;
 
-import frc.robot.commands.claw.ClawEjectCommand;
+import frc.robot.commands.Intake.IntakeEjectingCommand;
 import frc.robot.commands.elevator.ElevatorPIDCommand;
 import frc.robot.commands.pivotArm.PivotArmPIDCommand;
 import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.PivotArm;
 import frc.robot.subsystems.Claw;
 import static frc.robot.Constants.ElevatorConstants.ELEVATOR_SETPOINT_EXTEND;
@@ -16,13 +17,13 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 public class MidScoreShootingCommand extends SequentialCommandGroup {
     
     // TODO: Add new command sequence based off of Cube Shooting
-    public MidScoreShootingCommand(Elevator elevator, PivotArm pivotarm, Claw claw) {
+    public MidScoreShootingCommand(Elevator elevator, PivotArm pivotarm, Claw claw, Intake intake) {
         addCommands(
             new ParallelCommandGroup(
                 new ElevatorPIDCommand(elevator, ELEVATOR_SETPOINT_EXTEND), 
                 new PivotArmPIDCommand(pivotarm, PIVOT_ARM_SETPOINT_MID)
             ),
-            new ClawEjectCommand(claw)
+            new IntakeEjectingCommand(intake)
         );
     }
 }
